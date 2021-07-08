b83b96ead78c7245765c0ef870b31b04468204cf
Patrick Goddard <thund3rbox@gmail.com>
2021-07-07 20:38:36 -0700

    tooling

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('node-sass'));
+const del = require('del');
+
+
+gulp.task('styles', () => {
+    return gulp.src('sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css/'));
+});
+
+gulp.task('clean', () => {
+    return del([
+        'css/main.css',
+    ]);
+});
+
+gulp.task('watch', () => {
+  gulp.watch('sass/**/*.scss', (done) => {
+      gulp.series(['clean', 'styles'])(done);
+  });
+});
+
+gulp.task('default', gulp.series(['clean', 'styles']));
